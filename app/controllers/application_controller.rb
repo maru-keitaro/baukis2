c855ec6ce3353da82dd00a8a3c9db70f8efdb022
maru-keitaro <maru.keitaro@gmail.com>
2021-05-14 20:49:07 +0900

    scss, model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
+  
   layout :set_layout
 
-  private def set_layout
-    if params [:controller].match(%r{＼A(staff|admin|customer)/})
-      Regexp.last_match[1]
+  private 
+  def set_layout
+    if params[:controller].match(%r{\A(staff|admin|customer)/})
+        Regexp.last_match[1]
     else
-      "customer"
+        "customer"
     end
   end
 end
+
